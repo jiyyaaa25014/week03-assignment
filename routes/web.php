7b85f4efe9428f4d9475b47b22300960a8e6e3a9
LATIFAH FAUZIA <latifahfauzia87@gmail.com>
2025-10-20 15:16:50 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,50 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
-Route::view('/', 'pages.home');
-Route::view('/about', 'pages.about');
-Route::view('/team', 'pages.team');
-Route::view('/contact', 'pages.contact');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+| Halaman utama dan informasi Jeeycookie
+*/
 
+Route::view('/', 'pages.home')->name('home');
+Route::view('/about', 'pages.about')->name('about');
+Route::view('/contact', 'pages.contact')->name('contact');
+
+/*
+|--------------------------------------------------------------------------
+| Halaman Pembayaran
+|--------------------------------------------------------------------------
+| Menampilkan metode pembayaran Bank Jago & QRIS
+*/
+Route::view('/payment', 'pages.payment')->name('payment');
+
+/*
+|--------------------------------------------------------------------------
+| Program & Halaman Dinamis
+|--------------------------------------------------------------------------
+*/
 Route::get('/program/{id?}', function ($id = null) {
     return view('pages.program', ['id' => $id]);
-});
+})->name('program');
 
+/*
+|--------------------------------------------------------------------------
+| Redirect dan Fallback
+|--------------------------------------------------------------------------
+*/
 Route::redirect('/home', '/');
 
 Route::fallback(function () {
     return view('pages.404');
 });
+
+/*
+|--------------------------------------------------------------------------
+| Form Contact (POST)
+|--------------------------------------------------------------------------
+*/
+Route::post('/contact', [ContactController::class, 'send'])->name('contact.send');
